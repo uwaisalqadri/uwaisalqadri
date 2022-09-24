ddb21decb0a48ff3847dce0415d0d42cf774e7f7
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2022-09-24 19:07:48 +0800

    something's wrong with me

--- src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
+++ src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
@@ -5,13 +5,20 @@ fun createReadMe(
 ): String {
     return """
         
-    <table><tr><td valign="top" width="60%">
+    <table>
+    <tr>
+    
+    <td valign="top" width="60%">
     
     ## GitHub Activity
     <!-- githubActivity starts -->
     ${githubActivity.joinToString("\n\n") { "    $it" }}
     <!-- githubActivity ends -->
-    </td><td valign="top" width="40%">
+    
+    </td>
+    
+    </tr>
+    </table>
     
     <sub><a href="https://github.com/ZacSweers/ZacSweers/">Inspired by Zac Sweeners's auto-updating profile README with Kotlin Implementation.</a></sub>
   """.trimIndent()
