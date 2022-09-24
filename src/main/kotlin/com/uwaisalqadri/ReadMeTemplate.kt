1e7749e662b6f608bb7da67f2d194943ea160ec5
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2022-09-24 19:11:43 +0800

    work plis

--- src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
+++ src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
@@ -5,20 +5,14 @@ fun createReadMe(
 ): String {
     return """
         
-    <table>
-    <tr>
-    
-    <td valign="top" width="60%">
+    <table><tr><td valign="top" width="60%">
     
     ## GitHub Activity
     <!-- githubActivity starts -->
     ${githubActivity.joinToString("\n\n") { "    $it" }}
     <!-- githubActivity ends -->
     
-    </td>
-    
-    </tr>
-    </table>
+    </td></tr></table>
     
     <sub><a href="https://github.com/ZacSweers/ZacSweers/">Inspired by Zac Sweeners's auto-updating profile README with Kotlin Implementation.</a></sub>
   """.trimIndent()
