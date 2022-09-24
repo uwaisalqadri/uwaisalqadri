06985ae0e888961ccae1a05f94336610152e48d1
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2022-09-24 19:56:09 +0800

    why its not working

--- src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
+++ src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
@@ -1,17 +1,25 @@
 package com.uwaisalqadri
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 fun createReadMe(
     githubActivity: List<ActivityItem>
 ): String {
     return """
-        
     <table><tr><td valign="top" width="100%">    
-            
+
     ## GitHub Activity
 
-    ${githubActivity.joinToString("\n\n") { "**${it.timestamp}** - ${it.text}" }}
+    ${githubActivity.joinToString("\n") { "**${it.timestamp.formatDate("dd MMM, yy")}** - ${it.text}" }}
                 
     <sub><a href="https://github.com/ZacSweers/ZacSweers/">Inspired by Zac Sweeners's auto-updating profile README with Kotlin Implementation.</a></sub>
         
   """.trimIndent()
+}
+
+fun String.formatDate(format: String): String {
+    val date = if (this.isNotEmpty()) SimpleDateFormat("yyyy-MM-ddThh:mm:ssZ").parse(this) else Date()
+    val dateFormatter = SimpleDateFormat(format, Locale.getDefault())
+    return dateFormatter.format(date ?: Date())
 }
