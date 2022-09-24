53efb8ebe6fa6c79676c5ec9c7d9eb721fd86cad
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2022-09-24 20:10:46 +0800

    hope this works

--- src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
+++ src/main/kotlin/com/uwaisalqadri/ReadMeTemplate.kt
@@ -1,20 +1,16 @@
 package com.uwaisalqadri
 
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.util.*
-
 fun createReadMe(
     githubActivity: List<ActivityItem>
 ): String {
     return """
-    <table><tr><td valign="top" width="100%">    
+<table><tr><td valign="top" width="100%">    
 
-    ## GitHub Activity
+## GitHub Activity
 
-    ${githubActivity.joinToString("\n") { it.toString() }}
+${githubActivity.joinToString("\n") { it.toString() }}
                 
-    <sub><a href="https://github.com/ZacSweers/ZacSweers/">Inspired by Zac Sweeners's auto-updating profile README with Kotlin Implementation.</a></sub>
+<sub><a href="https://github.com/ZacSweers/ZacSweers/">Inspired by Zac Sweeners's auto-updating profile README with Kotlin Implementation.</a></sub>
         
   """.trimIndent()
 }
